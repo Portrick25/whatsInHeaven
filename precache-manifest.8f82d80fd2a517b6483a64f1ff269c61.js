a8f89431cd70713c5de4b6e253721e0fce8ff7ae
Ritik jha <31991503+Portrick25@users.noreply.github.com>
2020-10-13 23:47:01 +0530

    Updates

---
+++ precache-manifest.8f82d80fd2a517b6483a64f1ff269c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc67ceee2011e454d1dbf94e7ff2c855",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/index.html"
+  },
+  {
+    "revision": "eac0b68260849779e303",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/static/css/main.eab6b7df.chunk.css"
+  },
+  {
+    "revision": "6a194eee4ade1547db41",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/static/js/2.a29aca7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/static/js/2.a29aca7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac0b68260849779e303",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/static/js/main.f9c61489.chunk.js"
+  },
+  {
+    "revision": "b6b152f264ba875e51e7",
+    "url": "/https//:portrick25.github.io/whatsInHeaven/static/js/runtime-main.a15f3162.js"
+  }
+]);
